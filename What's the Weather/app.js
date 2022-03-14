3fb6cff4bcde41de2147a39f8a5842d663106da8
Jasmeet Singh <jasmeetsingh0918@gmail.com>
2022-03-14 15:31:38 +0530

    Weather App

--- What's the Weather/app.js
+++ What's the Weather/app.js
@@ -1,12 +1,35 @@
-// Init weather object
-const weather = new Weather("New Delhi", "india");
+// Initialising objects
 
-// Init UI object
+const storage = new Storage();
+
+const weatherLocation = storage.getLocationData();
+
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
+
 const ui = new UI();
 
 // We only want to display weather when the DOM loads
 // * get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
+
+// Change Location Event
+document.querySelector("#w-change-btn").addEventListener("click", (e) => {
+  const city = document.querySelector("#city").value;
+  const country = document.querySelector("#country").value;
+
+  // Change Location
+  weather.changeLocation(city, country);
+
+  // Set Location in Local Storage
+  storage.setLocationData(city, country);
+
+  // Get Weather and display weather of the changed location
+  getWeather();
+
+  // Closing the modal
+  const saveButton = document.querySelector("#w-change-btn");
+  saveButton.setAttribute("data-dismiss", "modal");
+});
 
 function getWeather() {
   weather
