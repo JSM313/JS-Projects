fd9acd6bbfb0a4d258dace87c243261a7ba3d0d5
Jasmeet Singh <jasmeetsingh0918@gmail.com>
2022-04-12 13:20:02 +0530

    form validation in modal section

--- bookmark keeper/script.js
+++ bookmark keeper/script.js
@@ -1,16 +1,10 @@
-const modal = document.querySelector("#modal");
-
-const modalShow = document.querySelector("#show-modal");
-
-const modalClose = document.querySelector("#close-modal");
-
-const bookmarkForm = document.querySelector("#bookmark-form");
-
-const websiteNameEl = document.querySelector("#website-name");
-
-const websiteUrlEL = document.querySelector("#website-url");
-
-const bookmarksContainer = document.querySelector("#bookmark-container");
+const modal = document.getElementById("modal");
+const modalShow = document.getElementById("show-modal");
+const modalClose = document.getElementById("close-modal");
+const bookmarkForm = document.getElementById("bookmark-form");
+const websiteNameEl = document.getElementById("website-name");
+const websiteUrlEl = document.getElementById("website-url");
+const bookmarksContainer = document.getElementById("bookmarks-container");
 
 // Function to dynamically add show modal class...
 
@@ -29,3 +23,42 @@ modalClose.addEventListener("click", () =>
 window.addEventListener("click", (e) =>
   e.target === modal ? modal.classList.remove("show-modal") : false
 );
+
+// Validate form...
+const validate = (nameValue, urlValue) => {
+  const expression =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+
+  const regex = new RegExp(expression);
+
+  if (!nameValue || !urlValue) {
+    alert("Please submit values for both fields.");
+    return false;
+  }
+
+  if (!urlValue.match(regex)) {
+    alert("Please provide a valid web address");
+    return false;
+  }
+
+  // Valid
+  return true;
+};
+
+// Handle data from form...
+const storeBookmark = (e) => {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteNameEl.value;
+
+  if (!urlValue.includes("http://", "https://")) {
+    urlValue = `https://${urlValue}.com`;
+  }
+
+  console.log(nameValue, urlValue);
+  if (!validate(nameValue, urlValue)) {
+    return false;
+  }
+};
+
+bookmarkForm.addEventListener("submit", storeBookmark);
